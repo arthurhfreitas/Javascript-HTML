89ba1eb6998cbb5417741e8394cc3e1ec8d72ab5
ARTHUR HENRIQUE DE FREITAS MARTINS <21810@unifacef.edu.br>
2019-08-21 21:45:19 -0300

    exemplo de vetor de objetos v 0.4

---
+++ vetobjeto.js
@@ -0,0 +1,16 @@
+function funcao (){
+    var vet = []
+    for(var i=0; i<5; i++){
+        var objeto = {}
+        // coloca dados no objeto
+        objeto.codigo - Number(prompt("Código"))
+        objeto.nome - (prompt("Nome"))
+        objeto.quantidade - Number(prompt("Quantidade"))
+        objeto.preco - Number(prompt("Preço"))
+        // coloca objeto no vetor
+        vet.push(objeto)
+
+    }
+
+}
+
